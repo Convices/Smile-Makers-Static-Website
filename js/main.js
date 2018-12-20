18f068bea5263b2ed955713d3ef3c296ce1f5477
VaisakhPradeep <92vaisakh92@gmail.com>
2018-12-20 20:10:54 +0530

    updates

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+
+	// $(".menu-icon").click(function(){
+	// 	$(".nav-list").toggleClass("selected");
+	// });
+
+	// $(".nav-list").click(function(){
+	// 	$(this).toggleClass("selected");
+	// });
+	
+	$(document).on('click', '.nav', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+	});
+
+	
+})
